cb66d7546a4fcad5fbf0488924eabe1a06a82aa4
Alejandro Padron <alejandro.padron@edosof.es>
2017-12-13 15:37:28 +0000

    Fix appModules Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,10 @@ import {AuthService} from './auth.service'
 import { environment } from '../environments/environment';
 import { Routes, RouterModule } from '@angular/router';
 
-<<<<<<< 3d6448538636242b33cb24bd3a709d5a6840fe9c
 import { environment } from '../environments/environment';
 import {AuthService} from './auth.service';
 import { FormsModule } from '@angular/forms';
-=======
 import { KothService } from './../providers/koth.service';
->>>>>>> Pull rebase DONE
 
 import { AppComponent } from './app.component';
 import { ScoreComponent } from './score/score.component';
@@ -46,27 +43,13 @@ var config = {
     BrowserModule,
     BrowserAnimationsModule,
     KothMaterialModule,
-<<<<<<< 3d6448538636242b33cb24bd3a709d5a6840fe9c
     AngularFireModule.initializeApp(environment.firebase),
-=======
-<<<<<<< 93531e7f787fd82e2e5453b90b1421a954e5b0fb
-    AngularFireModule.initializeApp(environment.firebase, 'my-app-name'),
->>>>>>> Pull rebase DONE
     AngularFireAuthModule,
     AngularFireDatabaseModule,
     RouterModule,
     FormsModule
   ],
   providers: [AuthService],
-=======
-    AngularFireModule.initializeApp(config),
-    RouterModule.forRoot(
-      routes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
-  ],
-  providers: [KothService],
->>>>>>> WIP leaderListComponent
   bootstrap: [AppComponent]
 })
 export class AppModule { }
