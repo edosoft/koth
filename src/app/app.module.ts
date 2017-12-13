4083ea42877ab259f5b5b431d25e419664ba2519
Luis Montero <luis.montero@edosoft.es>
2017-12-13 09:54:05 +0000

    Sign component generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SignComponent } from './sign/sign.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignComponent
   ],
   imports: [
     BrowserModule
