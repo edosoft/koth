3d6448538636242b33cb24bd3a709d5a6840fe9c
Alejandro Padron <alejandro.padron@edosof.es>
2017-12-13 13:00:30 +0000

    WIP Test LeaderList Component

--- src/app/leader-list/leader-list.component.ts
+++ src/app/leader-list/leader-list.component.ts
@@ -6,10 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leader-list.component.css']
 })
 export class LeaderListComponent implements OnInit {
+  records:any = [
+    {
+      user: 'pepe',
+      score: 20
+    },
+    {
+      user: 'juan',
+      score: 21
+    },
+    {
+      user: 'juana',
+      score: 9
+    },
+    {
+      user: 'hermana',
+      score: 232
+    }
+  ];
+
+  user:string;
 
   constructor() { }
 
   ngOnInit() {
+    
   }
+  sortJson(){
+    this.records.sort(function(a, b){
+      return b.score - a.score
+    });
+  }
+  isSorted(records){
+    for (let i = 0; i < records.length; i++) {
+      if (records[i].score < records[i+1].score){
+        return false
+    }
+    return true;
+  }
+
+
+
+
 
 }
