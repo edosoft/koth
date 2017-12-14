1ad751c78af2a14db12ab902efb55d1b32dc96cf
Daniel San <daniel.san@edosoft.es>
2017-12-14 06:55:02 +0000

    WIP get gameplay from database

--- src/app/score/score.component.ts
+++ src/app/score/score.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-score',
@@ -11,14 +12,18 @@ export class ScoreComponent implements OnInit {
   rank: number;
   record: number;
 
-  constructor() {
+  constructor(private auth: AuthService) {
     this.score = 0;
     this.rank = 0;
     this.record = 0;
   }
 
   ngOnInit() {
+    this.getUserInfo();
+  }
 
+  getUserInfo() {
+    const user = this.auth.currentUser;
   }
 
 }
