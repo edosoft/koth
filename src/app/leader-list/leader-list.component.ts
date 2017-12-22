eb32c3e06f51202b586c2e731113c39b1e39f1ce
alejandroangel <33858672+alejandroangel@users.noreply.github.com>
2017-12-22 12:34:58 +0000

    LeaderList (#10)
    * leader-list component
    
    * WIP Test LeaderList Component
    
    * Pull rebase DONE
    
    * Fix appModules Modules
    
    * Firebase Config
    
    * Modified table
    
    * leader-list component
    
    * WIP Test LeaderList Component
    
    * Pull rebase DONE
    
    * Fix appModules Modules
    
    * Modified table
    
    * Fix test from everybody
    
    * DELETE node_modules

---
+++ src/app/leader-list/leader-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import * as firebase from '@firebase/app';
+import { Router } from '@angular/router';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+
+
+@Component({
+  selector: 'app-leader-list',
+  templateUrl: './leader-list.component.html',
+  styleUrls: ['./leader-list.component.css']
+})
+
+export class LeaderListComponent implements OnInit {
+  records:any;
+  games: any;
+  user:string;
+  recordsLength:number;
+
+
+  constructor(private router: Router, db: AngularFireDatabase) {
+    this.records =  db.list('/games', ref => ref.orderByChild('score').limitToLast(10)).valueChanges();
+    console.log(this.records);
+  }
+
+  ngOnInit() {
+    this.records.subscribe( games => {
+      this.games = games.reverse();
+      this.recordsLength = this.games.length;
+    });
+  }
+}
