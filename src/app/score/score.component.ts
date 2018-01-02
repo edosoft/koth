25174401ecf958d3538fb3ae60bb8d2962151fb1
Daniel San <daniel.san@edosoft.es>
2018-01-02 08:40:49 +0000

    Score component Done.

--- src/app/score/score.component.ts
+++ src/app/score/score.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
-
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-score',
@@ -15,37 +15,38 @@ export class ScoreComponent implements OnInit {
   score: number;
   rank: number;
   record: number;
+  games: any;
   user_info: any;
   last_user_game: any;
   data  : Observable<any[]>;
   JSON = JSON;
-
+  currentUser:any;
   user = null;
 
-  constructor(private auth: AuthService, public db: AngularFireDatabase) {
-    this.score = 0;
-    this.rank = 0;
-    this.record = 0;
+  constructor(private auth: AuthService, public db: AngularFireDatabase, private route: Router, private zone: NgZone) {
+    this.games =  this.db.list('/games', ref => ref.orderByChild('score')).valueChanges();
   }
 
-ngOnInit() {
-  this.last_user_game = this.db.list('/games', ref => ref.orderByChild('email').equalTo('javier@test.com').limitToLast(1)).valueChanges();
-  this.score = this.last_user_game.score;
-  this.last_user_game.subscribe(item => {
-    this.score = item[0].score;
-  })
+  ngOnInit() {
+      this.currentUser = this.auth.currentUser;
+      this.last_user_game = this.db.list('/games', ref => ref.orderByChild('email').equalTo(this.currentUser.email).limitToLast(1)).valueChanges();
+      this.last_user_game.subscribe((score) => {
+        this.score = score[0].score;
+      });
+      this.games.subscribe((game) => {
+        game = game.reverse();
+        for (var i = 0; i < game.length; i++){
+          if (game[i].email == this.currentUser.email){
+            this.rank = i + 1;
+            this.record = game[i].score;
+            break;
+          }
+        }
+      })
+    }
 
-  this.user_info = this.db.list('/', ref => ref.orderByChild('email').equalTo('javier@test.com')).valueChanges();
-  this.user_info.subscribe(item => {
-    item.map(x => {
-      if (x.highscore > this.record){
-        this.record = x.highscore;
-      }
-      if (x.highrank > this.rank){
-        this.rank = x.highrank;
-      }
-    });
-  });
+  onPressRetry(){
+      this.route.navigate(['/game']);
   }
 
 }
